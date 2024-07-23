3d7bc0566ea38083bde69306daa23f083a21203e
AbhishekMahur06 <abhishekmahur05@gmail.com>
2024-07-23 16:09:37 +0530

    some changes

---
+++ src/data.js
@@ -0,0 +1,81 @@
+import p1 from "./assets/E-Bharat.png";
+import p2 from "./assets/Movix.png";
+import p3 from "./assets/Nike.png";
+import p4 from "./assets/Nike-Store.png";
+import p5 from "./assets/project-5.png";
+import p6 from "./assets/project-6.png";
+
+export const filters = [
+  {
+    name: "All",
+    id: "all",
+  },
+  {
+    name: "ReactJs",
+    id: "react",
+  },
+  {
+    name: "Tailwind CSS",
+    id: "tailwind",
+  },
+  {
+    name: "Bootstrap",
+    id: "bootstrap",
+  },
+];
+
+export const projects = [
+  {
+    name: "Join the Innovative Car Wash Service",
+    image: p1,
+    tags: ["react", "tailwind"],
+  },
+  {
+    name: "Your Luggage is in Safe Hands",
+    image: p2,
+    tags: ["react", "bootstrap"],
+  },
+  {
+    name: "The Art of Hiring",
+    image: p3,
+    tags: ["tailwind"],
+  },
+  {
+    name: "We Are Solving Global Problems",
+    image: p4,
+    tags: ["tailwind"],
+  },
+  {
+    name: "The English Tea Ceremony",
+    image: p5,
+    tags: ["bootstrap", "react"],
+  },
+  {
+    name: "Saving Photo Memories",
+    image: p6,
+    tags: ["bootstrap"],
+  },
+];
+
+export const achievements = [
+  {
+    id: 1,
+    year: 80,
+    description: "SUCCESSFULLY<br />COMPLETED<br />PROJECTS",
+  },
+  {
+    id: 2,
+    year: 15,
+    description: "YEARS OF<br />EXPERIENCE",
+  },
+  {
+    id: 3,
+    year: 25,
+    description: "TECHNOLOGIES",
+  },
+  {
+    id: 4,
+    year: 70,
+    description: "COMMUNITY<br />POSTS",
+  },
+];
